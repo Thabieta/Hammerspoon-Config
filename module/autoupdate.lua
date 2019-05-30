1d02f25a447fd43b91047d0595b903bc28f6eab9
hououinkami <hououinkami@gmail.com>
2019-05-30 16:24:13 +0800

    Create autoupdate.lua

---
+++ module/autoupdate.lua
@@ -0,0 +1,12 @@
+local cmdArr = {
+    "cd ~/.hammerspoon && git pull",
+}
+function shell(cmd)
+    result = hs.osascript.applescript(string.format('do shell script "%s"', cmd))
+end
+function runAutoScripts()
+    for key, cmd in ipairs(cmdArr) do
+        shell(cmd)
+    end
+end
+hs.timer.doEvery(86400, runAutoScripts)
