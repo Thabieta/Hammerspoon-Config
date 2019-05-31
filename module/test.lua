46d4d267ec7a377dafe8894ee59e4162575adfe8
hououinkami <hououinkami@gmail.com>
2019-05-31 13:30:45 +0800

    Update test.lua

--- module/test.lua
+++ module/test.lua
@@ -1,4 +1,12 @@
-
+local _,library,_ = hs.osascript.applescript([[tell application "iTunes" to get name of playlists]])
+local playlist = {}
+for i=7, #(library) do
+	table.insert(playlist, {title = library[i], fn = shuffleplay(library[i])})
+end
+function shuffleplay(playlistname)
+	local playscript = [[tell application "iTunes" to play playlist named pname]]
+	hs.osascript.applescript(playscript:gsub("pname", "playlistname"))
+end
 
 local abar = hs.menubar.new()
 function atime()
