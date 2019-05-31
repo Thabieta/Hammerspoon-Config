e8d796c338c54cfb559a8bfa28507b946bed7a88
hououinkami <hououinkami@gmail.com>
2019-05-31 12:15:49 +0800

    Create test.lua

---
+++ module/test.lua
@@ -0,0 +1,9 @@
+local _,library,_ = hs.osascript.applescript([[tell application "iTunes" to get name of playlists]])
+local playlist = {}
+local menulist = {}
+for i=7, #(library) do
+		--playlist.title = library[i]
+		--playlist.fn = "1"
+table.insert(menulist, {title = library[i], fn = "1"})
+	end
+print(playlist[1].title)
