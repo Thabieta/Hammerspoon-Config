0bba59ffc466e8157fca7358b67875502f5fd6e6
hououinkami <hououinkami@gmail.com>
2019-05-31 16:42:33 +0800

    Update window.lua

--- module/window.lua
+++ module/window.lua
@@ -1,37 +1,82 @@
 hs.window.animationDuration = 0
 local winhistory = {}
-local winhistory2 = {}
--- 记录窗口历史
-local function windowStash(window)
+-- 记录窗口初始位置
+function windowStash(window)
 	local winid = window:id()
 	local winf = window:frame()
-	-- 确认存储历史不超过50个
 	if #winhistory > 50 then
-		-- 移除最后一次历史
 		table.remove(winhistory)
 	end
 	local winstru = {winid, winf}
-	table.insert(winhistory, winstru)
-end
--- 记录窗口初始位置
-local function windowStash2(window)
-	local winid = window:id()
-	local winf = window:frame()
-	if #winhistory2 > 50 then
-		table.remove(winhistory2)
-	end
-	local winstru = {winid, winf}
+	-- table.insert(winhistory, winstru) 注释掉本栏目后面几行取消注释该行则为记录窗口历史
 	local exist = false
-	for idx,val in ipairs(winhistory2) do
+	for idx,val in ipairs(winhistory) do
 		if val[1] == winid then
 			exist = true
 		end
 	end
 	if exist == false then
-		table.insert(winhistory2, winstru) 
+		table.insert(winhistory, winstru) 
 	end
 end
 -- 窗口动作
+local cwin = hs.window.focusedWindow()
+if cwin then
+local cscreen = cwin:screen()
+local cres = cscreen:fullFrame()
+local wf = cwin:frame()
+local Resize = {}
+Resize.halfleft = function ()
+	windowStash(cwin)
+	cwin:setFrame({x=cres.x, y=cres.y, w=cres.w/2, h=cres.h})
+end
+Resize.halfright = function ()
+	windowStash(cwin)
+	cwin:setFrame({x=cres.x+cres.w/2, y=cres.y, w=cres.w/2, h=cres.h})
+end
+Resize.halfup = function ()
+	windowStash(cwin)
+	cwin:setFrame({x=cres.x, y=cres.y, w=cres.w, h=cres.h/2})
+end
+Resize.halfdown = function ()
+	windowStash(cwin)
+	cwin:setFrame({x=cres.x, y=cres.y+cres.h/2, w=cres.w, h=cres.h/2})
+end
+Resize.fullscreen = function ()
+	windowStash(cwin)
+	cwin:setFrame({x=cres.x, y=cres.y, w=cres.w, h=cres.h})
+end
+Resize.center = function ()
+	windowStash(cwin)
+	cwin:centerOnScreen()
+end
+Resize.reset = function ()
+	local cwin = hs.window.focusedWindow()
+	local cwinid = cwin:id()
+	for idx,val in ipairs(winhistory) do
+		if val[1] == cwinid then
+			cwin:setFrame(val[2])
+		end
+	end
+end
+end
+hotkey = require "hs.hotkey"
+hyper = {"ctrl", "alt"}
+function windowsManagement(keyFuncTable)
+	for key,fn in pairs(keyFuncTable) do
+		hotkey.bind(hyper, key, fn)
+	end
+end
+hotkey.bind(hyper, 'return', Resize.fullscreen)
+windowsManagement({
+		left = Resize.halfleft,
+		right = Resize.halfright,
+		up = Resize.halfup,
+		down = Resize.halfdown,
+		c = Resize.center,
+		delete = Resize.reset,
+	})
+--[[
 function Resize(option)
 	local cwin = hs.window.focusedWindow()
 	if cwin then
@@ -39,23 +84,31 @@ function Resize(option)
 		local cres = cscreen:fullFrame()
 		local wf = cwin:frame()
 		if option == "halfleft" then
-			windowStash2(cwin)
+			windowStash(cwin)
 			cwin:setFrame({x=cres.x, y=cres.y, w=cres.w/2, h=cres.h})
 		elseif option == "halfright" then
-			windowStash2(cwin)
+			windowStash(cwin)
 			cwin:setFrame({x=cres.x+cres.w/2, y=cres.y, w=cres.w/2, h=cres.h})
 		elseif option == "halfup" then
-			windowStash2(cwin)
+			windowStash(cwin)
 			cwin:setFrame({x=cres.x, y=cres.y, w=cres.w, h=cres.h/2})
 		elseif option == "halfdown" then
-			windowStash2(cwin)
+			windowStash(cwin)
 			cwin:setFrame({x=cres.x, y=cres.y+cres.h/2, w=cres.w, h=cres.h/2})
 		elseif option == "fullscreen" then
-			windowStash2(cwin)
+			windowStash(cwin)
 			cwin:setFrame({x=cres.x, y=cres.y, w=cres.w, h=cres.h})
 		elseif option == "center" then
-			windowStash2(cwin)
+			windowStash(cwin)
 			cwin:centerOnScreen()
+		elseif option == "reset" then
+			local cwin = hs.window.focusedWindow()
+			local cwinid = cwin:id()
+			for idx,val in ipairs(winhistory) do
+				if val[1] == cwinid then
+					cwin:setFrame(val[2])
+				end
+			end
 		elseif option == "expand" then
 			cwin:setFrame({x=wf.x-stepw, y=wf.y-steph, w=wf.w+(stepw*2), h=wf.h+(steph*2)})
 		elseif option == "shrink" then
@@ -78,21 +131,11 @@ function Undo()
 		end
 	end
 end
--- 重置回初始状态
-function Reset()
-	local cwin = hs.window.focusedWindow()
-	local cwinid = cwin:id()
-	for idx,val in ipairs(winhistory2) do
-		if val[1] == cwinid then
-			cwin:setFrame(val[2])
-		end
-	end
-end
-hs.hotkey.bind(hyper, 'right', function() Resize("halfright") end)
-hs.hotkey.bind(hyper, 'left', function() Resize("halfleft") end) 
-hs.hotkey.bind(hyper, 'up', function() Resize("halfup") end)
-hs.hotkey.bind(hyper, 'down', function() Resize("halfdown") end)
-hs.hotkey.bind(hyper, 'c', function() Resize("center") end)
-hs.hotkey.bind(hyper, 'return', function() Resize("fullscreen") end)
-hs.hotkey.bind(hyper, 'delete', function() Reset() end)
-------------------------------------------------------------------------------------------
+hotkey.bind(hyper, 'right', function() Resize("halfright") end)
+hotkey.bind(hyper, 'left', function() Resize("halfleft") end) 
+hotkey.bind(hyper, 'up', function() Resize("halfup") end)
+hotkey.bind(hyper, 'down', function() Resize("halfdown") end)
+hotkey.bind(hyper, 'c', function() Resize("center") end)
+hotkey.bind(hyper, 'return', function() Resize("fullscreen") end)
+hotkey.bind(hyper, 'delete', function() Resize("reset") end)
+--]]
