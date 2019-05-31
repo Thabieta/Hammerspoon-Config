f048eee8be2464192f2236c1ad96fc0cdabf298e
hououinkami <hououinkami@gmail.com>
2019-05-31 09:55:10 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,21 +1,10 @@
-hs.hotkey.alertDuration = 0
-hs.hints.showTitleThresh = 0
+-- Hammerspoon设置
 hs.preferencesDarkMode = true
--- 重新加载快捷键
-hsreload_keys = hsreload_keys or {{"cmd", "shift", "ctrl"}, "R"}
-if string.len(hsreload_keys[2]) > 0 then
-	hs.hotkey.bind(hsreload_keys[1], hsreload_keys[2], "Reload Configuration", function() hs.reload() end)
-end
--- 偏好设置快捷键
-hsopenPreferences_keys = hsopenPreferences_keys or {{"cmd", "shift", "ctrl"}, "p"}
-if string.len(hsopenPreferences_keys[2]) > 0 then
-	hs.hotkey.bind(hsopenPreferences_keys[1], hsopenPreferences_keys[2], "Open Preferences", function() hs.openPreferences() end)
-end
--- 控制台快捷键
-hsconsole_keys = hsconsole_keys or {"alt", "Z"}
-if string.len(hsconsole_keys[2]) > 0 then
-	hs.hotkey.bind(hsconsole_keys[1], hsconsole_keys[2], "Toggle Hammerspoon Console", function() hs.toggleConsole() end)
-end
+hotkey = require "hs.hotkey"
+hyperkey = {"cmd", "shift", "ctrl"}
+hotkey.bind(hyperkey, "R", hs.reload)
+hotkey.bind(hyperkey, "p", hs.openPreferences)
+hotkey.bind({"alt"}, "Z", hs.toggleConsole)
 -- 组件加载管理
 local module_list = {
 	"module.IME",
