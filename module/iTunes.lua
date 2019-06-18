9685339ea8dc6c056f4fe1f268430ce9cedbcc8a
hououinkami <hououinkami@gmail.com>
2019-06-18 10:04:22 +0800

    Update iTunes.lua

--- module/iTunes.lua
+++ module/iTunes.lua
@@ -267,9 +267,9 @@ function setitunesbar()
 	else -- 若iTunes没有运行
 		deletemenubar()
 	end
-	--hs.timer.doAfter(1, setitunesbar)
+	hs.timer.doAfter(1, setitunesbar)
 end
+setitunesbar()
 if iTunesBar ~= nil then
 	iTunesBar:setMenu(setmenu)
 end
-hs.timer.doEvery(1, setitunesbar)
